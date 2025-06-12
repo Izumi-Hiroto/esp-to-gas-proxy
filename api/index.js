f3ee1b6d59e58ad5cb10990874c0f746b107b77b
Izumi-Hiroto <s2331012hu@s.chibakoudai.jp>
2025-06-12 23:46:31 +0900

    Move API to pages/api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
-export default function handler(req, res) {
-  const msg = req.query.msg || "No message";
+export default async function handler(req, res) {
+  const msg = req.query.msg || 'no message';
 
-  const url = `https://script.google.com/macros/s/AKfycbyDMMRm-VD_Qn7fQQYcUyY_t2W0pajmZKmV-QN5uRKklUfEyIoDsO_56ewe8SmhDHjs3w/exec?msg=${encodeURIComponent(msg)}`;
+  try {
+    const url = `https://script.google.com/macros/s/AKfycbyDMMRm-VD_Qn7fQQYcUyY_t2W0pajmZKmV-QN5uRKklUfEyIoDsO_56ewe8SmhDHjs3w/exec?msg=${encodeURIComponent(msg)}`;
+    
+    const response = await fetch(url); // Å© node-fetch ÇégÇÌÇ∏ÅAïWèÄ fetch
 
-  res.writeHead(302, { Location: url });
-  res.end();
+    if (!response.ok) {
+      return res.status(response.status).send('Error from GAS');
+    }
+
+    const text = await response.text();
+    res.status(200).send(text);
+  } catch (error) {
+    res.status(500).send('Fetch error: ' + error.message);
+  }
 }
